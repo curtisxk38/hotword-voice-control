2f489266d298090aeb11058fcddc47e80b58eab5
curtisxk38 <ck2ha@virginia.edu>
2016-12-12 18:27:56 -0500

    modify gitignore, make api call

---
+++ main.py
@@ -0,0 +1,36 @@
+import json
+import sys
+import os
+import requests
+import test
+
+def load_credentials(name):
+    if os.path.isfile(name):
+        with open(name, "r") as f:
+            return json.load(f)
+    else:
+        return None
+    
+def api_call(audio_name, url, auth, params, headers):
+    with open(audio_name, "rb") as audio:
+        http_response = requests.post(url,
+                                      auth=auth,
+                                      data=audio,
+                                      params=params,
+                                      headers=headers,
+                                      stream=False)
+        return http_response
+    
+def main(file_name):
+    cred = load_credentials("credentials")
+    #url = cred["url"]
+    url = "https://stream.watsonplatform.net/speech-to-text/api/v1/recognize"
+    auth = (cred["username"], cred["password"])
+    params = {"continuous":True, "word_confidence":True}
+    headers = {"content-type": "audio/wav"}
+
+    r = api_call(file_name, url, auth, params, headers)
+    print(r.text)
+      
+if __name__ == "__main__":
+    main(sys.argv[1])
